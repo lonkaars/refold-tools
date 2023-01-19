9905b8cefdee18f2ed9b503ef27814e9545d8f61
lonkaars <loek@pipeframe.xyz>
2023-01-19 23:08:03 +0100

    move filters to variables in makefile

--- common.mk
+++ common.mk
@@ -1,8 +1,15 @@
+TO_SINGLE_LINE:=tr '\n' ' '
+REMOVE_DOUBLE_SLASH_COMMENTS:=sed 's/\/\/.*$$//g'
+REMOVE_SLASH_STAR_COMMENTS:=sed 's/\/\*[^\/]*\*\///g'
+REMOVE_TABS:=sed 's/\t//g'
+REMOVE_WHITESPACE:=sed 's/  */ /g'
+TRIM_WHITESPACE:=sed -E 's/^\s*(.*)\s+$$/\1/g'
+
 %.min.js: %.js
-	sed 's/\/\/.*$$//g' $< | tr '\n' ' ' | sed 's/\/\*[^\/]*\*\///g' | sed 's/\t//g' | sed 's/  */ /g' > $@
+	cat $< | $(REMOVE_DOUBLE_SLASH_COMMENTS) | $(TO_SINGLE_LINE) | $(REMOVE_SLASH_STAR_COMMENTS) | $(REMOVE_TABS) | $(REMOVE_WHITESPACE) | $(TRIM_WHITESPACE) > $@
 
 %.min.css: %.css
-	cat $< | tr '\n' ' ' | sed 's/\/\*[^\/]*\*\///g' | sed 's/\t//g' | sed 's/  */ /g' > $@
+	cat $< | $(TO_SINGLE_LINE) | $(REMOVE_SLASH_STAR_COMMENTS) | $(REMOVE_TABS) | $(REMOVE_WHITESPACE) | $(TRIM_WHITESPACE) > $@
 
 %.html: %.m4
 	m4 $< > $@
