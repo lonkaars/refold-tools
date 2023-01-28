c16a16342d8a2b3e06edbfd4af7fdac6a66bcb64
lonkaars <loek@pipeframe.xyz>
2023-01-28 10:43:15 +0100

    fix makefile REMOVE_SLASH_STAR_COMMENTS (allow "/" in /* */ comments)

--- common.mk
+++ common.mk
@@ -1,6 +1,6 @@
 TO_SINGLE_LINE:=tr '\n' ' '
 REMOVE_DOUBLE_SLASH_COMMENTS:=sed 's/\/\/.*$$//g'
-REMOVE_SLASH_STAR_COMMENTS:=sed 's/\/\*[^\/]*\*\///g'
+REMOVE_SLASH_STAR_COMMENTS:=sed 's/\/\*[^\*]*\*\///g'
 REMOVE_TABS:=sed 's/\t//g'
 REMOVE_WHITESPACE:=sed 's/  */ /g'
 TRIM_WHITESPACE:=sed -E 's/^\s*(.*)\s+$$/\1/g'
