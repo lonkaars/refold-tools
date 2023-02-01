e0653d60ca122df3c301ec0cfab91df15c8d224e
lonkaars <loek@pipeframe.xyz>
2023-02-01 18:21:13 +0100

    only run userscript on search page

--- yomichan/userscript-loader.js
+++ yomichan/userscript-loader.js
@@ -1,6 +1,10 @@
-(async () => eval((await yomichan.api.getSettings([{
-	scope: "profile",
-	optionsContext: { current: true },
-	path: 'general.userScript'
-}]))[0].result))();
+(async () => {
+	if (window.location.protocol != "chrome-extension:") return; // only run on yomichan pages
+	if (window.location.pathname != "/search.html") return; // only run on search page
+	eval((await yomichan.api.getSettings([{
+		scope: "profile",
+		optionsContext: { current: true },
+		path: 'general.userScript'
+	}]))[0].result);
+})();
 
