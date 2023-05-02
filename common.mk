f6c55448d48585ea366b2e2ce0d1784416593bb0
lonkaars <loek@pipeframe.xyz>
2023-05-02 11:57:49 +0200

    cleanup yomichan user folder

--- common.mk
+++ common.mk
@@ -1,3 +1,5 @@
+M4FLAGS += -I..
+
 TO_SINGLE_LINE:=tr '\n' ' '
 REMOVE_DOUBLE_SLASH_COMMENTS:=sed 's/\/\/.*$$//g'
 REMOVE_SLASH_STAR_COMMENTS:=sed 's/\/\*[^\*]*\*\///g'
@@ -12,7 +14,7 @@ TRIM_WHITESPACE:=sed -E 's/^\s*(.*)\s+$$/\1/g'
 	cat $< | $(TO_SINGLE_LINE) | $(REMOVE_SLASH_STAR_COMMENTS) | $(REMOVE_TABS) | $(REMOVE_WHITESPACE) | $(TRIM_WHITESPACE) > $@
 
 %: %.m4
-	m4 -I.. $< > $@
+	m4 $(M4FLAGS) $< > $@
 
 %.b64: %
 	base64 -w0 $< > $@
